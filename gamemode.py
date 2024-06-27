a5b5ece64dc0c200bd7841b5bf8782bd5e70ffc7
sanders93 <sanders93business@gmail.com>
2024-06-26 22:13:56 -0400

    gamemode class updated

--- gamemode.py
+++ gamemode.py
@@ -6,5 +6,29 @@ class GameMode:
     '''
     
     def __init__(self) -> None:
-        player_list: list[Player] = []
-        board: Board = Board()
+        self.player_list: list[Player] = []
+        self.board: Board = Board()
+        
+    def checkBoard(self, player: Player) -> bool:
+        '''
+        Checks if a player has won
+        '''
+        pass
+    def checkHorizontals(self) -> bool:
+        '''
+        Checks the horizontals of the board
+        '''
+        for i in range(9, 3):
+            if self.board.pieces[i] == self.board.pieces[i + 1] == self.board.pieces[i + 2]:
+                return True
+        return False
+    
+    def checkVerticals(self) -> bool:
+        '''
+        Checks the verticals of the board
+        '''
+       # for i in range(9, 3):
+            
+    
+    def checkDiagonals() -> bool:
+        pass
